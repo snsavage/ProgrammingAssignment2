0f8f97beb7f9a46da73b438e5b62e6bb3bc96333
Scott Savage <snsavage@gmail.com>
2014-09-18 21:17:53 -0400

    First Draft of Project.  Needs comments.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,40 @@
 ## functions do
 
 ## Write a short comment describing this function
+## <<- searches for a variable in the parent envirnments for assignment and if
+##      not found, will assign the variable in the global environment.  
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        get <- function() x
+        
+        setInverse <- function(solve) inverse <<- solve
+        getInverse <- function() inverse
+        
+        list(set = set, get = get, 
+                setInverse = setInverse, getInverse = getInverse)
+       
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        inverse <- x$getInverse()
+        
+        if(!is.null(inverse)) {
+                message("Getting chased data...")
+                return(inverse)
+        }
+        
+        data <- x$get()
+        inverse <- solve(data, ...)
+        x$setInverse(inverse)
+        inverse
+        
 }
